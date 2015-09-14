a896255b70de9f3499dcf5356ed72ec788475fe6
Steven Smith <stevensmithmsc@hotmail.com>
2015-09-14 18:32:04 +0000

    Lecture 104

---
+++ Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveRate = 5f;
+	public float padding = 1f;
+	public float health = 250;
+	
+	private float maxLeft = -5f;
+	private float maxRight = 5f;
+	
+	// Use this for initialization
+	void Start () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+		maxLeft = leftmost.x + padding;
+		maxRight = rightmost.x - padding;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * moveRate * Time.deltaTime;
+		} else if(Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += Vector3.right * moveRate * Time.deltaTime;
+		}
+		// restirct the player to the gamespace
+		float newX = Mathf.Clamp(transform.position.x, maxLeft, maxRight);
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+}
