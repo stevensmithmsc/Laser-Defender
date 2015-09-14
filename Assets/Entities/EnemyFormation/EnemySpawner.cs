140081d40fbb005754d37da821416afafdcdbaf8
Steven Smith <stevensmithmsc@hotmail.com>
2015-09-14 19:54:09 +0000

    Lecture 106

--- Assets/Entities/EnemyFormation/EnemySpawner.cs
+++ Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -4,6 +4,13 @@ using System.Collections;
 public class EnemySpawner : MonoBehaviour {
 
 	public GameObject enemyPrefab;
+	public float width = 10f;
+	public float height = 5f;
+	public float speed = 5f;
+	
+	private Vector3 direction = Vector3.left;
+	private float maxLeft = -5f;
+	private float maxRight = 5f;
 
 	// Use this for initialization
 	void Start () {		
@@ -11,10 +18,26 @@ public class EnemySpawner : MonoBehaviour {
 			GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
 			enemy.transform.parent = child;
 		}
+		
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+		maxLeft = leftmost.x + (width/2);
+		maxRight = rightmost.x - (width/2);
+	}
+	
+	public void OnDrawGizmos(){
+		Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.position += direction * speed * Time.deltaTime;
+		
+		if (transform.position.x > maxRight) {
+			direction = Vector3.left;
+		} else if (transform.position.x < maxLeft) {
+			direction = Vector3.right;
+		}	
 	}
 }
